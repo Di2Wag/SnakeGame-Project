b6700d28bb28839399cab9af4a4edec52bc5903d
pedro goncalves <pedro.sng@gmail.com>
2025-02-03 21:19:54 +0000

    edit snake movement logic

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,5 +1,6 @@
 package game;
 
+import com.codeforall.online.simplegraphics.graphics.Text;
 import field.Field;
 import fruits.Fruit;
 import snake.Snake;
@@ -11,8 +12,16 @@ public class Game {
     private Snake snake;
     private Fruit[] fruits;
     private Fruit fruit;
+    private Direction direction;
+    private int threadSleepUnits = 300;
+    private int score = 0;
+    private Text scoreDisplay;
 
     public Game(Field field, Snake snake) {
+        scoreDisplay = new Text(10,10,"Score: " + score);
+        scoreDisplay.draw();
+
+        direction = Direction.values()[(int)(Math.random()*Direction.values().length)];
         this.field = field;
         this.snake = snake;
         createFruits();
@@ -40,14 +49,76 @@ public class Game {
 
     public void deleteFruit () {
            for (int i = 0; i < fruits.length; i++) {
+
                if (snake.getRow() == fruits[i].getRow() && snake.getCol() == fruits[i].getCol()) {
                    fruit.getFruitPic().delete();
+                   updateScore();
                    fruits[i] = new Fruit(field);
-
                }
 
            }
 
+    }
+
+    private void updateScore(){
+        System.out.println("updates core");
+        score += 10;
+        System.out.println(score);
+        scoreDisplay.setText("Score: " + score);
+        updateSpeed();
+    }
+
+    private void updateSpeed(){
+
+        if(score > 10) {
+            threadSleepUnits = 100;
+        }
+
+    }
+
+    public void start() throws InterruptedException {
+
+        while (!snake.isCrashed()){
+
+            Thread.sleep(threadSleepUnits);
+
+            moveInDirection();
+
+        }
+
+    }
+
+    private void moveInDirection(){
+
+        switch (direction) {
+
+            case UP:
+                snake.moveUp();
+                break;
+            case DOWN:
+                snake.moveDown();
+                break;
+            case RIGHT:
+                snake.moveRight();
+                break;
+            case LEFT:
+                snake.moveLeft();
+                break;
+        }
+
+        eatingFruitAndGrowing();
+
+    }
+
+    private void eatingFruitAndGrowing(){
+        if(checkCollisionWithFruits()){
+            snake.growSnake();
+            deleteFruit();
+        }
+    }
+
+    public void setDirection(Direction direction){
+        this.direction = direction;
     }
 
 }
